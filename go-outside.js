b58c73259e46752d34f426b9efd0d423f51b68d0
Tiffany Kwok <tiffany.s.kwok@gmail.com>
2017-04-17 23:19:07 +0000

    Adds go-outside function exercise

---
+++ go-outside.js
@@ -0,0 +1,23 @@
+var raining = false;
+var cold = true;
+var temperature = 12;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+
+if (temperature < -40 || temperature > 40) {
+  console.log ("Maybe going outside isn't such a great idea...");
+}
+else if (temperature < 0) {
+  console.log ("Make sure you pick out a scarf!");
+}
+else if (temperature < 15) {
+  console.log ("Short sleeves won't cut it!")
+}
+else {
+  console.log ("Short sleeves are fine.");
+}
+
+console.log ("Now you're ready to go outside!");
