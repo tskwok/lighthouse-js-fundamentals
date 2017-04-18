f1ff7cff353c7c1de554aa8f343c79d7c338df10
Tiffany Kwok <tiffany.s.kwok@gmail.com>
2017-04-18 01:07:04 +0000

    Adds loopylighthouse2.js file

---
+++ loopylighthouse2.js
@@ -0,0 +1,23 @@
+var num;
+
+function loopyLighthouse (range, multiples, words)
+{
+  for (num = range[0]; num <= range[1]; num++)
+  {
+    if (num % multiples[0] === 0 && num % multiples[1] === 0){
+      console.log (words[0] + words[1]);
+    }
+    else if (num % multiples[0] === 0){
+      console.log (words[0]);
+    }
+    else if (num % multiples[1] === 0) {
+      console.log (words[1]);
+    }
+    else{
+      console.log (num);
+    }
+  }
+}
+
+
+loopyLighthouse([15,90], [2,5], ["Batty", "Beacon"]);
