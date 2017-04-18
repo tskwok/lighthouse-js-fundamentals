2289ee4b165156a02d8e9f6523f4605b584e8b66
Tiffany Kwok <tiffany.s.kwok@gmail.com>
2017-04-18 00:25:30 +0000

    Adds chorus-which and packing-list function exercises

---
+++ chorus-while.js
@@ -0,0 +1,17 @@
+var chorus = "Let's dance!";
+var repeat = 0;
+// while (repeat < 10) {
+//   if (repeat === 5){
+//     console.log ("*change key*");
+//   }
+//   console.log(chorus);
+//   repeat++;
+//   console.log (repeat);
+// }
+
+for (var repeat = 0; repeat < 10; repeat ++)
+{
+  console.log (chorus);
+  console.log (repeat);
+}
+console.log("Until the sun comes up!");
---
+++ packing-list.js
@@ -0,0 +1,6 @@
+var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen Stuff to Pack:");
+for (var i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
